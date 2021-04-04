eb89e5c2db82e58ce0c565e61a41685156f29fd9
Gabriel Batista <gabriel_batista@outlook.com>
2021-04-04 19:11:52 -0300

    (WIP) :hammer: implementation of the third scenario

--- FuzzBuzz/FuzzBuzz.Tests/FuzzBuzzTests.cs
+++ FuzzBuzz/FuzzBuzz.Tests/FuzzBuzzTests.cs
@@ -35,7 +35,7 @@ namespace FuzzBuzz.Tests
         /// Than fill up the output varible with the string "Buzz"
         /// </summary>
         [Test]
-        public void FuzzBuzz_WhenModOfFiveEqualsZero_ReturnsStringFuzz(){
+        public void FuzzBuzz_WhenModOfFiveEqualsZero_ReturnsStringBuzz(){
             //Arrange
             int input = 5;
             string output = string.Empty;
@@ -47,6 +47,34 @@ namespace FuzzBuzz.Tests
 
             //Assert
             Assert.AreEqual("Buzz", output);
+        }
+
+        /// <summary>
+        /// Given an integer input
+        /// <para />
+        /// When the mod of the division by three is equal zero
+        /// <para />
+        /// And also the mod of the division by five is equal zero
+        /// <para />
+        /// Than fill up the output varible with the string "FuzzBuzz"
+        /// </summary>
+        [Test]
+        public void FuzzBuzz_WhenModOfThreeAndModOfFiveEqualsZero_ReturnsStringFuzzBuzz(){
+            //Arrange
+            int input = 15;
+            string output = string.Empty;
+
+            //Act
+            if(input %3 == 0){
+                output += "Fuzz";
+            }
+
+            if(input %5 == 0){
+                output += "Buzz";
+            }
+
+            //Assert
+            Assert.AreEqual("FuzzBuzz", output);
         }
     }
 }
